a0ea112f713308056b76718e10dfa8a6cf0b99e6
zyan <ww9720@gmail.com>
2022-03-25 20:49:35 +0900

    Add java project

---
+++ java/src/com/practice/chapter2/Constant.java
@@ -0,0 +1,4 @@
+package com.practice.chapter2;
+
+public class Constant {
+}
---
+++ java/src/com/practice/chapter3_innerclass_lambda_stream/lambda.java
@@ -0,0 +1,10 @@
+package com.practice.chapter3_innerclass_lambda_stream;
+
+public class lambda {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "World";
+
+
+    }
+}
