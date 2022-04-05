6872906e0ffb3e7baf0da0f45d6fe2763d540e0e
zzking <freedate@swallaby.com>
2022-04-05 14:09:07 +0900

    [Add] querydsl 환경설정

---
+++ querydsl/src/main/java/com/example/demo/study1/find.java
@@ -0,0 +1,13 @@
+package com.example.demo.study1;
+
+import jakarta.persistence.EntityManager;
+
+public class find {
+    public static void main(String[] args) {
+
+    }
+
+    void queryDsl() {
+
+    }
+}
